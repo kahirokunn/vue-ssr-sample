e5a525a9300c5a55c15ab4f19bc427cb1dab3d9d
k-okina <okinakahiro@gmail.com>
2019-05-11 11:03:05 +0900

    [add] make minimum ssr sample. vue is not working on the brower yet.

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const Vue = require("vue");
+const server = require("express")();
+const renderer = require("vue-server-renderer").createRenderer();
+
+server.get("*", (req, res) => {
+  const app = new Vue({
+    data: () => ({ count: 0 }),
+    computed: {
+      url: () => req.url
+    },
+    methods: {
+      inc() {
+        this.count++;
+      },
+      dec() {
+        this.count--;
+      }
+    },
+    template: `
+      <div>
+        <div>The visited URL is: {{ url }}</div>
+        <p><span>Count: {{ count }}</span></p>
+        <button @click="inc()">+</button>
+        <button @click="dec()">-</button>
+      </div>`
+  });
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      res.status(500).end("Internal Server Error");
+      return;
+    }
+    res.end(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><title>Hello</title></head>
+        <body>${html}</body>
+      </html>
+    `);
+  });
+});
+
+const port = 8888;
+server.listen(port);
+
+/* eslint-disable no-console */
+console.log(`port number is ${port}`);
